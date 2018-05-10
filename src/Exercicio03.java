9bae02753a3bc97732926c6a41a41faca37d11b4
Leonardo Moreira <leonardomoreira050@gmail.com>
2018-05-10 11:19:11 -0300

    Finalização Exercicio 03

--- src/Exercicio03.java
+++ src/Exercicio03.java
@@ -1,9 +1,23 @@
+import javax.swing.JOptionPane;
 
 public class Exercicio03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String vazio = "";
+		double total = 0;
+		int quantidade = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe a quantidade de regitros"));
+		double precos[] = new double[quantidade];
+		for (int i = 0; i < precos.length; i++) {
+			precos[i] = Double.parseDouble(JOptionPane.showInputDialog(null, "Informe o " + (i + 1) + "º" + " preco"));
+		}
+		for (int i = 0; i < quantidade; quantidade++) {
+			quantidade += precos[i];
+		}
+		for (int i = 0; i < precos.length; i++) {
+			total += precos[i];
+		}
+		
+		JOptionPane.showMessageDialog(null, "Total de precos: \n" + total);
 	}
 
 }
