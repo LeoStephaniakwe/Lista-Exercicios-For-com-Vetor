00a48f61a8a9415488320d65765f95aa9c3a7b3c
Leonardo Moreira <leonardomoreira050@gmail.com>
2018-05-10 09:22:18 -0300

    Exercicio02 Finalizado

--- src/Exercicio02.java
+++ src/Exercicio02.java
@@ -1,12 +1,16 @@
 import javax.swing.JOptionPane;
+
 public class Exercicio02 {
 
 	public static void main(String[] args) {
 		int[] idade = new int[7];
+		String vazio = "";
 		for(int i = 0; i<7; i++){
-			idade[i] = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a idade:"));
+			idade[i]= Integer.parseInt(JOptionPane.showInputDialog(null, "Digite a idade:"));
+			vazio += idade[i] + "\n";
 		}
-
+		JOptionPane.showMessageDialog(null,"Idades informadas:\n" + vazio);
 	}
+	
 
 }
--- src/ExercicioForComVetor.java
+++ src/ExercicioForComVetor.java
@@ -2,9 +2,9 @@ import javax.swing.JOptionPane;
 public class ExercicioForComVetor {
 
 	public static void main(String[] args) {
-		String nome[] = new String[25];
+		String nome[] = new String[3];
 		
-		for(int i=0 ; i<25; i++){
+		for(int i=0 ; i<3; i++){
 			nome[i] = JOptionPane.showInputDialog(null, "Digite o nome que deseja armazenar:");
 			
 		}
