9610db16877af03b80fa84522f460b2386402944
Leonardo Moreira <leonardomoreira050@gmail.com>
2018-05-10 08:07:29 -0300

    Exercicio01

---
+++ src/ExercicioForComVetor.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+public class ExercicioForComVetor {
+
+	public static void main(String[] args) {
+		String nome[] = new String[25];
+		
+		for(int i=0 ; i<25; i++){
+			nome[i] = JOptionPane.showInputDialog(null, "Digite o nome que deseja armazenar:");
+			
+		}
+		
+
+	}
+
+}
